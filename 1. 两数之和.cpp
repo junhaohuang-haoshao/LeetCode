a156c7680fc40bdf1919db83ef4e97a9e740aad0
junhaohuang-haoshao <87748355+junhaohuang-haoshao@users.noreply.github.com>
2021-07-21 14:15:32 +0800

    Add files via upload

---
+++ 1. 两数之和.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        int i,j;
+        vector<int> ans;
+        for(i = 0; i < nums.size()-1; ++i)
+        {
+            for(j = i+1; j < nums.size(); ++j)
+            {
+                if(nums[i]+nums[j] == target)
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
